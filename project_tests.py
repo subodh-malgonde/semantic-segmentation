Fixed project_tests for batch normalization

project_tests.py
@@ -86,7 +86,8 @@ def test_layers(layers):
     vgg_layer3_out = tf.placeholder(tf.float32, [None, None, None, 256])
     vgg_layer4_out = tf.placeholder(tf.float32, [None, None, None, 512])
     vgg_layer7_out = tf.placeholder(tf.float32, [None, None, None, 4096])
-    layers_output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+    is_training = tf.placeholder(tf.bool)
+    layers_output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes, is_training)
 
     _assert_tensor_shape(layers_output, [None, None, None, num_classes], 'Layers Output')
 